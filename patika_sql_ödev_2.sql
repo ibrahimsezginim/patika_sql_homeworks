041dc59e5e2c4bafb89bfbc027ab6a463577f68f
İBRAHİM SEZGİN <118987715+ibrahimsezginim@users.noreply.github.com>
2022-12-25 01:44:36 +0300

    Update patika_sql_ödev_2.sql

--- patika_sql_ödev_2.sql
+++ patika_sql_ödev_2.sql
@@ -1,3 +1,4 @@
+https://app.patika.dev/ibrahimsezgin
 /*
 1-film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 
 16.99 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)
