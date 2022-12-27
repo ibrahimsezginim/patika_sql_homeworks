dea199c0042d43cc57cf7e836b5ab6291e9953a8
İBRAHİM SEZGİN <118987715+ibrahimsezginim@users.noreply.github.com>
2022-12-28 00:18:49 +0300

    Update patika_sql_ödev_7.sql

--- patika_sql_ödev_7.sql
+++ patika_sql_ödev_7.sql
@@ -1,11 +1,12 @@
+https://app.patika.dev/ibrahimsezgin
 /*
-film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+1-film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
 SELECT rating, COUNT(*) FROM film
 GROUP BY rating;
 */
 
 /*
-film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan 
+2-film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan 
 replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
 SELECT replacement_cost, COUNT(*) FROM film
 GROUP BY replacement_cost
@@ -21,7 +22,7 @@ GROUP BY store_id
 
 
 /*
-4. city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla 
+4-city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla 
 şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
 SELECT country_id, COUNT(*) FROM city
 GROUP BY country_id
