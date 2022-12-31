189c19a81a765621a4c5fdbcd2ecf1e1ad9cf576
İBRAHİM SEZGİN <118987715+ibrahimsezginim@users.noreply.github.com>
2023-01-01 01:57:06 +0300

    Update patika_sql_ödev_8.sql

--- patika_sql_ödev_8.sql
+++ patika_sql_ödev_8.sql
@@ -1,23 +1,22 @@
 https://app.patika.dev/ibrahimsezgin
-/*
-1-test veritabanınızda employee isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE, 
-email VARCHAR(100) olan bir tablo oluşturalım.
+
+
+--1-test veritabanınızda employee isimli sütun bilgileri id(INTEGER), name VARCHAR(50), birthday DATE, email VARCHAR(100) olan bir tablo oluşturalım.
 CREATE TABLE employe(
 	id SERIAL PRIMARY KEY,
 	first_name VARCHAR(50),
 	birthday DATE,
 	email VARCHAR(50)
 );
-*/
 
 
-/*
-2-Oluşturduğumuz employee tablosuna 'Mockaroo' servisini kullanarak 50 adet veri ekleyelim.
-*/
 
 
-/*
-3-Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.
+--2-Oluşturduğumuz employee tablosuna 'Mockaroo' servisini kullanarak 50 adet veri ekleyelim.
+
+
+
+--3-Sütunların her birine göre diğer sütunları güncelleyecek 5 adet UPDATE işlemi yapalım.
 UPDATE employe
 SET first_name = 'Kai'
 WHERE first_name = 'Niki'
@@ -29,16 +28,14 @@ WHERE first_name = 'Tanya';
 UPDATE employe
 SET email = 'amberhahahah@asfdasdf.com'
 WHERE first_name = 'Amber'
-*/
 
 
-/*
-4-Sütunların her birine göre ilgili satırı silecek 5 adet DELETE işlemi yapalım.
+
+--4-Sütunların her birine göre ilgili satırı silecek 5 adet DELETE işlemi yapalım.
 DELETE FROM employe WHERE email='amberhahahah@asfdasdf.com';
 DELETE FROM employe WHERE birthday='1997-05-13';
 DELETE FROM employe WHERE first_name='Kai';
-*/
 
-/*
-Veritabanında istediğimiz tabloyu silmek için DELETE FROM table_name; kullanabiliriz
-*/
+
+
+--Veritabanında istediğimiz tabloyu silmek için DELETE FROM table_name; kullanabiliriz
