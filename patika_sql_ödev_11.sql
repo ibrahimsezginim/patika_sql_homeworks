a0d0dbb6b7fd35dfa6d2160edbfdbf74e65d0fa7
İBRAHİM SEZGİN <118987715+ibrahimsezginim@users.noreply.github.com>
2022-12-31 16:40:11 +0300

    Add files via upload

---
+++ patika_sql_ödev_11.sql
@@ -0,0 +1,38 @@
+/*
+1-actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+(SELECT first_name FROM customer)
+UNION ALL
+(SELECT first_name FROM actor)
+*/
+
+
+/*
+2-actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+(SELECT first_name FROM customer)
+INTERSECT
+(SELECT first_name FROM actor)
+*/
+
+
+/*
+3-actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+(SELECT first_name FROM customer)
+EXCEPT
+(SELECT first_name FROM actor)
+*/
+
+
+/*
+4-İlk 3 sorguyu tekrar eden veriler için de yapalım.
+(SELECT last_name FROM customer)
+UNION ALL
+(SELECT last_name FROM actor)
+
+(SELECT last_name FROM customer)
+INTERSECT
+(SELECT last_name FROM actor)
+
+(SELECT last_name FROM customer)
+EXCEPT
+(SELECT last_name FROM actor)
+*/
